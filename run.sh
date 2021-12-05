fc69f0dce29a3ba6f4400d9f4af8260a8bfca59a
Jonathan James <jonathan.james@fundingcircle.com>
2021-12-05 20:24:46 +0000

    Preserve .bash_history

--- run.sh
+++ run.sh
@@ -5,10 +5,12 @@ ScriptDir="$(cd "$(dirname "$0")"; pwd)"
 source "${ScriptDir}/shared.sh"
 
 main() {
+    touch "${ScriptDir}/.bash_history"
     docker run --hostname 'soc-2021' \
                --interactive \
                --rm \
                --tty \
+               --volume "${ScriptDir}/.bash_history:/root/.bash_history" \
                --volume "${ScriptDir}/sources:/root/sources" \
                --volume "${GemVolume}:/usr/local/bundle" \
                "$Tag" \
