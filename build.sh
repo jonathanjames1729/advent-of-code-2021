97dba1e4bf86fa27a2cee4edcdcd5076298816fb
Jonathan James <jonathan.james@fundingcircle.com>
2021-12-18 04:09:45 +0000

    Docker improvements

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ declare -r ScriptDir
 source "${ScriptDir}/shared.sh"
 
 main() {
+    docker pull ruby:2.7.5-bullseye
     docker build --no-cache \
                  --tag "$Tag" \
                  "$ScriptDir"
--- run.sh
+++ run.sh
@@ -6,16 +6,27 @@ source "${ScriptDir}/shared.sh"
 
 main() {
     touch "${ScriptDir}/.bash_history"
-    docker run --hostname 'soc-2021' \
-               --interactive \
-               --rm \
-               --tty \
-               --volume "${ScriptDir}/.bash_history:/root/.bash_history" \
-               --volume "${ScriptDir}/sources:/root/sources" \
-               --volume "${GemVolume}:/usr/local/bundle" \
-               --volume "${EmacsVolume}:/root/.emacs.d" \
-               "$Tag" \
-               /bin/bash
+
+    local container_id
+    container_id="$(docker ps -f ancestor="$Tag" -q)"
+
+    if [ -z "$container_id" ]; then
+        docker run --hostname 'soc-2021' \
+                   --interactive \
+                   --rm \
+                   --tty \
+                   --volume "${ScriptDir}/.bash_history:/root/.bash_history" \
+                   --volume "${ScriptDir}/sources:/root/sources" \
+                   --volume "${GemVolume}:/usr/local/bundle" \
+                   --volume "${EmacsVolume}:/root/.emacs.d" \
+                   "$Tag" \
+                   /bin/bash
+    else
+        docker exec --interactive \
+                    --tty \
+                    "$container_id" \
+                    /bin/bash
+    fi
 }
 
 main "$@"
