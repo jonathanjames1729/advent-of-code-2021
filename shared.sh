16f300abdba75f97f596c384337d8b9cec95c201
Jonathan James <jonathan.james@fundingcircle.com>
2021-12-25 13:30:58 +0000

    Fix docker tags

--- shared.sh
+++ shared.sh
@@ -1,4 +1,4 @@
 
-declare -r Tag='summer-of-code-2021'
-declare -r GemVolume='summer-of-code-2021-gems'
-declare -r EmacsVolume='summer-of-code-2021-emacs'
+declare -r Tag='advent-of-code-2021'
+declare -r GemVolume='advent-of-code-2021-gems'
+declare -r EmacsVolume='advent-of-code-2021-emacs'
